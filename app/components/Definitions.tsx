7a46b72c17d193d3ed9a2ee1d2704df775ebb817
Adel <adel@MacBook-Air-Adel.local>
2024-01-16 17:08:06 +0300

    added first slide

--- app/components/Definitions.tsx
+++ app/components/Definitions.tsx
@@ -2,22 +2,19 @@ import Image from 'next/image'
 
 export default function Definitions() {
     return (
-        <>
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark: to-sfsecondary-new1-700 before:dark:opacity-10 after:dark: from-sfsecondary-new1-200 after:dark:via-[#c8102e] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-            <h1 className="  text-center text-4xl text-white m-3">SF Economy Express (International) Definitions</h1>
-        </div>
-        <div className='text-center m-3 font-miller '>
-        {/* <h1 className='text-3xl m-3'>SF Economy Express (International) Definitions</h1> */}
-            <p>SF Economy Express (International) is an international economy express delivery serivce launched for customers who need ship middle/high-value commodities and who are demanding on transit time and service</p>
+        <div className=' md:mt-36 text-center m-3 font-miller '>
+            <div className="h-3 bg-sfsecondary-new1-700 w-screen my-24"></div>
+            <h1 className=" font-miller  text-center text-4xl">SF Economy Express (International) Definitions</h1>
+            <p className='m-6'>SF Economy Express (International) is an international economy express delivery serivce launched for customers who need ship middle/high-value commodities and who are demanding on transit time and service</p>
        
         <div className='grid sm:grid-cols-1 md:grid-cols-3 gap-3 text-center m-3 font-miller'>
             
-            <div className=" border-dashed  group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-sfsecondary-new2-300 hover:bg-gray-100 hover:dark:border-sfsecondary-new2-600 hover:dark:bg-neutral-800/30">
+            <div className=" border-dashed border-sfsecondary-new1-600 group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-sfsecondary-new2-300 hover:bg-gray-100 hover:dark:border-sfsecondary-new2-600 hover:dark:bg-neutral-800/30">
                 <h2 className={`mb-3 text-2xl font-semibold`}>Netherlands</h2>
                 <Image
                         src="/company.svg"
                         alt="company"
-                        className="dark:invert mx-auto"
+                        className="dark:invert mx-auto my-3"
                         width={45}
                         height={45}
                         priority
@@ -26,7 +23,7 @@ export default function Definitions() {
                     <Image
                         src="/store.svg"
                         alt="store"
-                        className="dark:invert mx-auto"
+                        className="dark:invert mx-auto my-3"
                         width={45}
                         height={45}
                         priority
@@ -35,82 +32,100 @@ export default function Definitions() {
                     <Image
                         src="/people.svg"
                         alt="people"
-                        className="dark:invert mx-auto"
+                        className="dark:invert mx-auto my-3"
                         width={45}
                         height={45}
                         priority
                         />
                         <h3>Consumer</h3>
             </div>
-            <div className=" border-dashed  group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-sfsecondary-new2-300 hover:bg-gray-100 hover:dark:border-sfsecondary-new2-600 hover:dark:bg-neutral-800/30">
+            <div className="  border-dashed border-sfsecondary-new1-600 group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-sfsecondary-new2-300 hover:bg-gray-100 hover:dark:border-sfsecondary-new2-600 hover:dark:bg-neutral-800/30">
                 <h2 className={`mb-3 text-2xl font-semibold`}>
                 Delivery to SF AMS Warehouse{' '}
                 </h2>
-                <Image
-                        src="/warehouse.svg"
-                        alt="warehouse"
-                        className="dark:invert mx-auto"
-                        width={45}
-                        height={45}
-                        priority
-                        />
-                <h3>SF AMS Warehouse</h3>
-                    <Image
+                <div className='grid grid-cols-2 my-auto'>
+                    <div>
+                        <Image
+                            src="/warehouse.svg"
+                            alt="warehouse"
+                            className="dark:invert mx-auto my-3"
+                            width={45}
+                            height={45}
+                            priority
+                            />
+                        <h3>SF AMS Warehouse</h3>
+                    </div>
+                    <div>
+                        <Image
                         src="/customs.svg"
                         alt="customs"
-                        className="dark:invert mx-auto"
+                        className="dark:invert mx-auto my-3"
                         width={45}
                         height={45}
                         priority
                         />
                         <h3>Customs Clearence</h3>
+                    </div>
+                    
                 </div>
-                <div className=" border-dashed  group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-sfsecondary-new2-300 hover:bg-gray-100 hover:dark:border-sfsecondary-new2-600 hover:dark:bg-neutral-800/30">
+                
+                </div>
+                <div className=" border-dashed border-sfsecondary-new1-600 group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-sfsecondary-new2-300 hover:bg-gray-100 hover:dark:border-sfsecondary-new2-600 hover:dark:bg-neutral-800/30">
                 <h2 className={`mb-3 text-2xl font-semibold`}>
                 Transit hub in China of SF{' '}
                 </h2>
-                <Image
-                        src="/warehouse.svg"
-                        alt="warehouse"
-                        className="dark:invert mx-auto"
-                        width={45}
-                        height={45}
-                        priority
-                        />
-                <h3>China</h3>
-                <Image
-                        src="/warehouse.svg"
-                        alt="warehouse"
-                        className="dark:invert mx-auto"
-                        width={45}
-                        height={45}
-                        priority
-                        />
-                <h3>HK Hub</h3>
+                <div className='grid grid-cols-2 my-auto'>
+                    <div>
+                            <Image
+                                src="/warehouse.svg"
+                                alt="warehouse"
+                                className="dark:invert mx-auto my-3"
+                                width={45}
+                                height={45}
+                                priority
+                                />
+                        <h3>China</h3>
+                        <Image
+                                src="/warehouse.svg"
+                                alt="warehouse"
+                                className="dark:invert mx-auto my-3"
+                                width={45}
+                                height={45}
+                                priority
+                                />
+                        <h3>HK Hub</h3>
+                    </div>
+                    <div>
                     <Image
-                        src="/customs.svg"
-                        alt="customs"
-                        className="dark:invert mx-auto"
+                        src="/home1.svg"
+                        alt="home"
+                        className="dark:invert mx-auto my-3"
                         width={45}
                         height={45}
                         priority
                         />
                     <h3>China Mainland</h3>
                     <Image
-                        src="/customs.svg"
-                        alt="customs"
-                        className="dark:invert mx-auto"
+                        src="/home1.svg"
+                        alt="home"
+                        className="dark:invert mx-auto my-3"
                         width={45}
                         height={45}
                         priority
                         />
                     <h3>Other Countries</h3>
+                    </div>
+          
                 </div>
-            <h3>Features of Commodies</h3>
+               
+                  
+                </div>
+            
+        </div>
+            <h3>Features of Commodies:</h3>
             <p>middle/high value</p>
             <p>time-senstive</p>
+        
         </div>
-        </div>
-        </>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import Slide1 from './components/Slide1'
 import Definitions from './components/Definitions'
 
 export default function Home() {
-  document.documentElement.classList.add('dark');
+  // const doc = document.documentElement.classList.add('dark')
+  // document.documentElement.classList.add('dark');
   return (
     <>
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark: to-sfsecondary-new1-700 before:dark:opacity-10 after:dark: from-sfsecondary-new1-200 after:dark:via-[#c8102e] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div className="h-[50vh] relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark: to-sfsecondary-new1-700 before:dark:opacity-10 after:dark: from-sfsecondary-new1-200 after:dark:via-[#c8102e] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
       {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"> */}
         {/* <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] "
@@ -86,7 +87,7 @@ export default function Home() {
       
    
     {/* <Slide1 /> */}
-    {/* <Definitions /> */}
+    <Definitions />
     </>
   )
 }
