b1c2d2de3aaeea0f59a87b3a58c70c8f9bf546b9
Adel <adel@MacBook-Air-Adel.local>
2024-01-19 13:04:01 +0300

    changed goodbye font

--- app/components/Goodbye.tsx
+++ app/components/Goodbye.tsx
@@ -3,7 +3,7 @@ export default function Goodbye() {
         <>
             
             <div className="flex flex-col justify-center h-[70vh] w-screen bg-gradient-to-tr from-sfprimary-new1-900 to-sfprimary-new1-700">
-                <h3 className=" text-sfsecondary-new2-300 font-dunhill text-center text-7xl m-3">Thank you!</h3>
+                <h3 className=" text-sfsecondary-new2-300 text-center text-7xl m-3">Thank you!</h3>
             </div>
         </>
     )
