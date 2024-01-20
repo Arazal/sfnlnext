c538edf9b78fc3976708ecda9d7a4a3b36164032
Adel <adel@MacBook-Air-Adel.local>
2024-01-20 13:07:09 +0300

    added speedinsights package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -29,6 +29,7 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Analytics />
+          <SpeedInsights />
           <Footer />
         </body>
     </html>
